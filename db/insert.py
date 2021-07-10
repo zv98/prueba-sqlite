fb8ff7e684a6b1708c81593f5b209f62b4fa7e2f
Julián Z <85649454+zv98@users.noreply.github.com>
2021-07-09 20:58:57 -0400

    Update insert.py

--- db/insert.py
+++ db/insert.py
@@ -1,6 +1,6 @@
 import sqlite3
 con = sqlite3.connect('arqui.db')
 cur = con.cursor()
-cur.execute("INSERT INTO usuariomascota VALUES (1,1)")
+cur.execute("INSERT INTO usuariomascota(idusuario,idmascota) VALUES (1,1)")
 con.commit()
 con.close()
