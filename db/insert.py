d8a544eaf2e6844838a0f9ade1c8c1efc5b3e16d
Julián Z <85649454+zv98@users.noreply.github.com>
2021-07-09 20:54:15 -0400

    xd

--- db/insert.py
+++ db/insert.py
@@ -1,5 +1,6 @@
 import sqlite3
 con = sqlite3.connect('arqui.db')
-cursor.execute("INSERT INTO usuariomascota VALUES (1,1)")
+cur = con.cursor()
+cur.execute("INSERT INTO usuariomascota VALUES (1,1)")
 con.commit()
 con.close()
